b6d67d7a62a5aba8642afcce5a183c3ab56bf208
radex <radexpl@gmail.com>
2019-09-27 10:57:53 +0200

    Make zacs/index free of ES6 (to simplify build)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// @flow
+/* eslint-disable */
 
-// TODO: Add Flow types
-
-const displayZacsError = (type: string) => (...args: any[]) => (): any => {
-  // eslint-disable-next-line no-console
-  console.log(`zacs error info`, args)
-  throw new Error(
-    `zacs.${type} method called directly (not transpiled). Your Babel file is probably misconfigured or you have a syntax error. See https://github.com/Nozbe/zacs#troubleshooting for more info`,
-  )
+function displayZacsError(methodName) {
+  return function zacsRuntimePlaceholder() {
+    return function zacsPlaceholderComponent() {
+      console.log(`zacs arguments passed:`, arguments)
+      throw new Error(
+        'zacs.' + methodName + 'method called directly (not transpiled). Your Babel file is probably misconfigured or you have a syntax error. See https://github.com/Nozbe/zacs#troubleshooting for more info',
+      )
+    }
+  }
 }
 
-export default {
+module.exports = {
   view: displayZacsError('view'),
   text: displayZacsError('text'),
   styled: displayZacsError('styled'),
