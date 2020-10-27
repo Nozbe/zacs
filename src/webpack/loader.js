fb5a9a1580d6c63e93b8a357371eeb0d2d53d903
radex <radexpl@gmail.com>
2020-10-27 11:41:04 +0100

    prettier

--- src/webpack/loader.js
+++ src/webpack/loader.js
@@ -2,10 +2,10 @@
 // https://github.com/callstack/linaria/blob/master/src/loader.ts
 // https://github.com/callstack/linaria/blob/f1cd66d0b68e8abb5e855819a6fb14019debdf06/src/transform.ts#L43
 
-const fs =require( 'fs');
-const path =require( 'path');
-const mkdirp =require( 'mkdirp');
-const normalize =require( 'normalize-path');
+const fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const normalize = require('normalize-path')
 const loaderUtils = require('loader-utils')
 
 exports.default = function loader(source) {
@@ -28,39 +28,34 @@ exports.default = function loader(source) {
   const cacheDirectory = '.zacs-cache'
   const outputFilename = normalize(
     path.join(
-      path.isAbsolute(cacheDirectory)
-        ? cacheDirectory
-        : path.join(process.cwd(), cacheDirectory),
+      path.isAbsolute(cacheDirectory) ? cacheDirectory : path.join(process.cwd(), cacheDirectory),
       this.resourcePath.includes(root)
         ? path.relative(root, baseOutputFileName)
-        : baseOutputFileName
-    )
-  );
+        : baseOutputFileName,
+    ),
+  )
 
+  // Read the file first to compare the content
+  // Write the new content only if it's changed
+  // This will prevent unnecessary WDS reloads
+  let currentCssText
 
-    // Read the file first to compare the content
-    // Write the new content only if it's changed
-    // This will prevent unnecessary WDS reloads
-    let currentCssText;
+  // TODO: We should be using input/output fs provided to webpack, but it breaks tests for some reason
+  try {
+    currentCssText = fs.readFileSync(outputFilename, 'utf-8')
+  } catch (e) {
+    // Ignore error
+  }
 
-    // TODO: We should be using input/output fs provided to webpack, but it breaks tests for some reason
-    try {
-      currentCssText = fs.readFileSync(outputFilename, 'utf-8');
-    } catch (e) {
-      // Ignore error
-    }
-
-    if (currentCssText !== cssText) {
-      // TODO: Remove unnecessary dependency
-      mkdirp.sync(path.dirname(outputFilename));
-      fs.writeFileSync(outputFilename, cssText);
-    }
-
+  if (currentCssText !== cssText) {
+    // TODO: Remove unnecessary dependency
+    mkdirp.sync(path.dirname(outputFilename))
+    fs.writeFileSync(outputFilename, cssText)
+  }
 
   const requireStatement = `require(${loaderUtils.stringifyRequest(this, outputFilename)})`
   // TODO: This breaks source maps
   const cleanSource = source.replace(match[0], requireStatement)
-
 
   this.callback(
     null,
