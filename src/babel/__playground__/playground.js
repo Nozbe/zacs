d3290a5a1bd7bdf33bccc5a556f2f7f7bfdfa7ce
radex <radexpl@gmail.com>
2022-09-01 12:47:28 +0200

    clean up playground

--- src/babel/__playground__/playground.js
+++ src/babel/__playground__/playground.js
@@ -6,30 +6,10 @@ const Root2 = zacs.createView([style.root, style.root2])
 const view = <Root />
 
 const styles = zacs.stylesheet({
-  one: {
-    margin: 5,
-    padding: 5,
-    inset: 5,
-  },
-  oneArray: {
-    margin: [5],
-    padding: [5],
-    inset: [5],
-  },
-  twoArray: {
-    margin: [5, 10],
-    padding: [5, 10],
-    inset: [5, 10],
-  },
   threeArray: {
     margin: [5, 10, 15],
     padding: [5, 10, 15],
     inset: [5, 10, 15],
-  },
-  fourArray: {
-    margin: [5, 10, 15, 20],
-    padding: [5, 10, 15, 20],
-    inset: [5, 10, 15, 20],
   },
   border: {
     border: [1, 'solid', 'red'],
