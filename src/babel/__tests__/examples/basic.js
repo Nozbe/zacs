ff11f629b02943f06f03622524daf6298e52afb8
radex <radexpl@gmail.com>
2023-03-03 11:07:55 +0100

    clean up tests, part 9

--- src/babel/__tests__/examples/basic.js
+++ src/babel/__tests__/examples/basic.js
@@ -23,22 +23,3 @@ function AnotherComponent() {
 
   return <Root2>{helper}</Root2>
 }
-
-/* === Component attributes === */
-
-// custom props -- different JSX syntaxes (will be cut out on web)
-const customProps = (
-  <Root foo={foo} isHighlighted={true} isBlocked bar="bar" Component={<Hello />} />
-)
-
-// DOM-safe attributes
-const domSafeAttributes = <Root onClick={onPress} href="http://example.com" data-foo="foo" />
-
-// React attributes
-const reactAttributes = <Root key={element.id} ref={reference} />
-
-// legacy nozbe styled components hack
-const styledhack = <Root __forwardedRef={__forwardedRef} />
-
-// react splat
-const splat = <Root2 {...props} />
---
+++ src/babel/__tests__/examples/webSafeAttributes.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import zacs from 'zacs'
+
+/* === Component attributes === */
+
+const Root = zacs.view(style.root)
+
+// custom props -- different JSX syntaxes (will be cut out on web)
+const customProps = (
+  <Root foo={foo} isHighlighted={true} isBlocked bar="bar" Component={<Hello />} />
+)
+
+// DOM-safe attributes
+const domSafeAttributes = <Root onClick={onPress} href="http://example.com" data-foo="foo" />
+
+// React attributes
+const reactAttributes = <Root key={element.id} ref={reference} />
+
+// legacy nozbe styled components hack
+const styledhack = <Root __forwardedRef={__forwardedRef} />
+
+// react splat
+const splat = <Root2 {...props} />
