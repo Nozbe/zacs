a4bf600785dcfed8db45c998b5d743950bbbfd87
radex <radexpl@gmail.com>
2023-03-03 13:40:49 +0100

    jest: update snapshot format

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,4 @@ module.exports = {
   // collectCoverageFrom: ['!**/node_modules/**', 'src/**'],
   // coverageDirectory: 'coverage',
   // coverageReporters: ['html', 'json'],
-  snapshotFormat: {
-    escapeString: true,
-    printBasicPrototype: true,
-  },
 }
