921a6155ada57aae3ae940a262589b9e0dcb1e03
radex <radexpl@gmail.com>
2023-03-09 14:54:44 +0100

    tests: simplify

--- src/babel/__tests__/examples/zacs.createX.js
+++ src/babel/__tests__/examples/zacs.createX.js
@@ -37,23 +37,6 @@ export const ExportedCombo = zacs.createStyled(
   ['comboProp1', 'comboProp2', 'zacs:inherit', 'ref'],
 )
 
-// regression test
-export const ExporterZacsStyleInherit = zacs.createView(style.foo, null, null, [
-  'zacs:style',
-  'zacs:inherit',
-])
-
 // Make sure nothing breaks if I use created component in the same file
 
-const combo = <ExportedCombo />
-const combo_props = <ExportedCombo foo bar={bar} height={100} ref={comboRef} />
-const combo_styles = <ExportedCombo zacs:inherit={props} />
-const combo_styles2 = <ExportedCombo zacs:inherit={props} zacs:style={{ width: 500 }} />
-
-// Pretend we're using an exported component
-
-const ImportedComponent2 = require('ImportedComponent')
-const passZacsStyledWithoutDef = <ImportedComponent2 zacs:style={{ width: 199 }} />
-const passZacsStyledWithoutDefInherit = (
-  <ImportedComponent2 zacs:inherit={props} zacs:style={{ width: 199 }} />
-)
+const combo = <ExportedCombo isFoo={true} />
