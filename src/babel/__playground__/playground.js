bc9d0265a5ca8d8f1c8e92c6c9edaed1db5086bc
radex <radexpl@gmail.com>
2023-03-09 14:55:27 +0100

    playground: clear

--- src/babel/__playground__/playground.js
+++ src/babel/__playground__/playground.js
@@ -1,17 +1,2 @@
 /* eslint-disable */
 import zacs from '@nozbe/zacs'
-
-const Root = zacs.view([style.root, style.root2])
-const Root2 = zacs.createView([style.root, style.root2])
-const view = <Root />
-
-const styles = zacs.stylesheet({
-  threeArray: {
-    margin: [5, 10, 15],
-    padding: [5, 10, 15],
-    inset: [5, 10, 15],
-  },
-  border: {
-    border: [1, 'solid', 'red'],
-  },
-})
