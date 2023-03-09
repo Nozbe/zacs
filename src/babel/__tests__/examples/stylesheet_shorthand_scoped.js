051cdff5aa2b3886c898558e7086b09a944f7d0b
radex <radexpl@gmail.com>
2023-03-09 13:49:26 +0100

    Improve confusing test

--- src/babel/__tests__/examples/stylesheet_shorthand_scoped.js
+++ src/babel/__tests__/examples/stylesheet_shorthand_scoped.js
@@ -1,11 +1,13 @@
 import * as zacs from '@nozbe/zacs'
 
 const styles = zacs.stylesheet({
-  scoped: {
+  scope1: {
     web: {
       margin: [5, 10],
       border: [1, 'solid', 'red'],
     },
+  },
+  scope2: {
     native: {
       margin: [5, 10],
       border: [1, 'solid', 'red'],
